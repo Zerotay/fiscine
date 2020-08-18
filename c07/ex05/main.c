e963cb71ae0b703cd1b5d601e4e7f42ca03a0491
Zerotay <67823010+Zerotay@users.noreply.github.com>
2020-08-18 22:59:10 +0900

    d

--- c07/ex05/main.c
+++ c07/ex05/main.c
@@ -5,8 +5,8 @@ char **ft_split(char *str, char *charset);
 
 int main(void)
 {
-	char str[] = "osfosdfbjjo";
-	char charset[5] = "o,";
+	char str[] = " ";
+	char charset[11] = "123";
 	char **result = ft_split(str, charset);
 	for(int i = 0; result[i]; i++)
 		printf("%s\n", result[i]);
--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -15,7 +15,7 @@
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
-void	ft_putchar(char *str);
+void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
 
